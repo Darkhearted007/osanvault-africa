4e085ae38889f75d0137dec2159d448c8c77ea97
Darkhearted007 <olugbenga1000@gmail.com>
2025-09-11 22:51:34 +0100

    Full Òsánvault deployment

--- scripts/nginx-service.sh
+++ scripts/nginx-service.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 while true; do
-    if ! pgrep -x nginx > /dev/null; then
+    if ! pgrep -x nginx >/dev/null; then
         echo "[INFO] Starting Nginx..."
         nginx -c /data/data/com.termux/files/home/www/osanvault-africa/nginx.conf
     fi
-    sleep 10
+    sleep 5
 done
